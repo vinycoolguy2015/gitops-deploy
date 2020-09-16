45f7775964c4a567cbc6d1dc718084ab3966483d
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2020-09-16 12:20:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello wonderful world!\n');
+  res.send('Hello wonderful world2!\n');
 });
 
 app.listen(8080, function () {
